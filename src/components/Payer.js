2120a0e20eea2b676ef5206c7f9e671ac47152e8
MatiCanzani <matiascanzani@gmail.com>
2024-09-29 14:29:15 +0100

    delete player

--- src/components/Payer.js
+++
@@ -1,9 +0,0 @@
-import Plyr from 'plyr';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const player = new Plyr('#player', {
-    autoplay: true,
-    muted: true,
-    loop: true,
-  });
-});
